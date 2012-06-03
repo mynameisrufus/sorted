629d1d2b9ddafb86c837f2254d06f1a6a2b0d713
Rufus Post <rufuspost@gmail.com>
2012-06-03 13:56:04 +1000

    fix railtie

--- lib/sorted/railtie.rb
+++ lib/sorted/railtie.rb
@@ -4,10 +4,12 @@ module Sorted
   class Railtie < Rails::Railtie
     initializer "sorted.configure" do |app|
       ActiveSupport.on_load :active_record do
-        ::ActiveRecord::Base.send :include, Sorted::Orms::ActiveRecord
+        require 'sorted/orms/active_record'
+        include Sorted::Orms::ActiveRecord
       end
       ActiveSupport.on_load :action_view do
-        ::ActionView::Base.send :include, Sorted::ViewHelpers::ActionView
+        require 'sorted/view_helpers/action_view'
+        include Sorted::ViewHelpers::ActionView
       end
     end
   end
